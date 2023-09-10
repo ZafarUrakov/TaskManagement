5f142264e039dce1187c76987c8021e9aecdaf71
Zafar <icom@MACKBOOK>
2023-09-10 10:17:27 +0300

    FOUNDATION: View Tasks

---
+++ TaskManagement/Managers/TaskManager.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Putting Tasks In Order 
+// --------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Brokers;
+using Local = TaskManagement.Models;
+
+namespace TaskManagement.Managers
+{
+    internal partial class TaskManager
+    {
+        private List<Local.Task> tasks = new List<Local.Task>();
+        ValueManipulator valueManipulator = new ValueManipulator();
+
+        public void ViewTasks()
+        {
+            Console.Clear();
+            Console.WriteLine("Tasks:");
+            foreach(var task in tasks)
+            {
+                Console.WriteLine($"ID: {task.Id}, Title: {task.Title}, " +
+                    $"DueData: {task.DueDate}, Completed: {task.IsCompleted}");
+            }
+        }
+    }
+}
