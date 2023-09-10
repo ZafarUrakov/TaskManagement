5090552a5c169f2b88d9cca2f81d2fcb6027461c
Zafar <icom@MACKBOOK>
2023-09-10 10:38:39 +0300

    FOUNDATION: Add Task

--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -3,12 +3,41 @@
 // Putting Tasks In Order 
 // --------------------------------------------------------;
 
+using TaskManagement.Brokers;
+using TaskManagement.Managers;
+
 namespace TaskManagement
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var report = new Report();
+            var taskManager = new TaskManager();
+            Console.WriteLine("Welcome to Task Manager!");
+
+            while (true)
+            {
+                int menuSelection = report.ShowMenu();
+
+                switch(menuSelection)
+                {
+                    case 1:
+                        taskManager.ViewTasks(); break;
+                    case 2:
+                        taskManager.AddTask(); break;
+                    case 3:
+                        taskManager.UpdateTask(); break;
+                    case 5:
+                        Console.WriteLine("Goodbye!");
+                        return;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid input. Please select a valid option.");
+                        break;
+                }
+
+            }
         }
     }
 }
