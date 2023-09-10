8a170d2d069172197d5accdaa6216fee5edc6d11
Zafar <icom@MACKBOOK>
2023-09-10 07:54:14 +0300

    INFRA: Create Build Project

---
+++ TaskManagement.Build/Program.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Putting Tasks In Order 
+// --------------------------------------------------------
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Build & Test CashOverflow",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+      {
+          {
+              "build",
+              new Job
+              {
+                  RunsOn = BuildMachines.WindowsLatest,
+
+                  Steps = new List<GithubTask>
+                  {
+                      new CheckoutTaskV2
+                      {
+                          Name = "Check out"
+                      },
+
+                      new SetupDotNetTaskV1
+                      {
+                          Name = "Setup .Net",
+
+                          TargetDotNetVersion = new TargetDotNetVersion
+                          {
+                              DotNetVersion = "7.0.400",
+                              IncludePrerelease = true
+                          }
+                      },
+
+                      new RestoreTask
+                      {
+                          Name = "Restore"
+                      },
+
+                      new DotNetBuildTask
+                      {
+                          Name = "Build"
+                      },
+
+                      new TestTask
+                      {
+                          Name = "Test"
+                      }
+                  }
+              }
+          }
+      }
+};
+var aDotNetClient = new ADotNetClient();
+string buildScriptPath = "../../../../.github/workflows/build.yml";
+string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
