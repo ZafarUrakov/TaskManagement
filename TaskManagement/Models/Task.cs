ae7857b7305a63afcb8734b2e8d818ec7353c024
Zafar <icom@MACKBOOK>
2023-09-10 07:43:24 +0300

    FOUNDATION: Task Add

---
+++ TaskManagement/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Models
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }   
+    }
+}
