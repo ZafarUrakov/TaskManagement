7119d6d2d13f3da36f99bcb44342441352f720ac
Zafar <icom@MACKBOOK>
2023-09-10 08:14:44 +0300

    INFRA: Add Program File

---
+++ TaskManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Putting Tasks In Order 
+// --------------------------------------------------------;
+
+namespace TaskManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
