68f197c50745d993a55b3b56e6a2c8124f3dc2b3
Zafar <icom@MACKBOOK>
2023-09-10 16:32:27 +0300

    FOUNDATION: Delete View Task

---
+++ TaskManagement/Managers/TaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Putting Tasks In Order 
+// --------------------------------------------------------;
+using Local = TaskManagement.Models;
+
+namespace TaskManagement.Managers
+{
+    public static class TaskExtensions
+    {
+        internal static void RemoveTask(this List<Local.Task> tasks, Local.Task task) =>
+            tasks.Remove(task);
+    }
+}
---
+++ TaskManagement/Managers/TaskManager.Delete.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Putting Tasks In Order 
+// --------------------------------------------------------;
+
+namespace TaskManagement.Managers
+{
+    public partial class TaskManager
+    {
+        public void DeleteTask()
+        {
+            if (tasks.Count > 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Loading...");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Enter the Id of the task to delete: ");
+                if (int.TryParse(Console.ReadLine()!, out int id))
+                {
+                    var taskToDelete = tasks.FirstOrDefault(x => x.Id == id);
+                    if (taskToDelete != null)
+                    {
+                        tasks.RemoveTask(taskToDelete);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Task deleted successfully.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Task not found.");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a valid task Id.");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sorry, but you have not added tasks yet. Add at least one!");
+                Console.ResetColor();
+            }
+        }
+    }
+}
