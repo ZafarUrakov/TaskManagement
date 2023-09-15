5db71bbf84fb54f9e4c7c71d4d5f5926363e8127
Zafar <icom@MACKBOOK>
2023-09-15 21:40:45 +0300

    CODE RUB: Run Code Clean Up

--- TaskManagement/Managers/Interfaces/ITaskManager.cs
+++ TaskManagement/Managers/Interfaces/ITaskManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskManagement.Managers.Interfaces
+﻿namespace TaskManagement.Managers.Interfaces
 {
     public interface ITaskManager
     {
